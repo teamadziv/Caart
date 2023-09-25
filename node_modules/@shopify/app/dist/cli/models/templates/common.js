c5a5dc7390b13c1af01ecd1031d18e3a07e2f8a0
sajidthedev <116505695+sajidthedev@users.noreply.github.com>
2023-09-25 22:52:53 +0500

    updated

---
+++ node_modules/@shopify/app/dist/cli/models/templates/common.js
@@ -0,0 +1,25 @@
+export function uiFlavors(path) {
+    return [
+        {
+            name: 'TypeScript',
+            value: 'typescript',
+            path,
+        },
+        {
+            name: 'JavaScript',
+            value: 'vanilla-js',
+            path,
+        },
+        {
+            name: 'TypeScript React',
+            value: 'typescript-react',
+            path,
+        },
+        {
+            name: 'JavaScript React',
+            value: 'react',
+            path,
+        },
+    ];
+}
+//# sourceMappingURL=common.js.map
